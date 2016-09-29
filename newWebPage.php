4e55c7ee5728edda01af31266c4391a408e58f08
Heiko Meyer <heiko.meyer@students.fhnw.ch>
2016-09-29 15:01:36 +0200

    Formulartag hinzugefügt

--- newWebPage.php
+++ newWebPage.php
@@ -10,8 +10,10 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <form>
         <?php
         // put your code here
         ?>
+        </form>
     </body>
 </html>
