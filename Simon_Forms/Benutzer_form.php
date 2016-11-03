f74b64abf57402e04edb25251e8ce17e225f40a8
bobsburgers11 <simon.zahnd@students.fhnw.ch>
2016-11-03 22:17:59 +0100

    Online_Verwaltungstool_Mehrfamilienhaus für VP neu hochgeladen

---
+++ Simon_Forms/Benutzer_form.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <form action="Rechnung_form.php" method="POST">
+    <h2>Benutzer erfassen</h2>
+    <table border="0" cellspacing="0" cellpadding="2">
+    <tbody>
+    <tr>
+        <td>Vorname:*</td>
+        <td>
+          <input type="text" name="name" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Nachname:*</td>
+        <td>
+            <input type="text" name="nachname" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Benutzername:*</td>
+        <td>
+           <input type="text" name="benutzername" value="" /> 
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Passwort:*</td>
+        <td>
+            <input type="password" name="passwort1" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Passwort bestätigen:*</td>
+        <td>
+           <input type="password" name="passwort2" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>E-Mail:*</td>
+        <td>
+            <input type="email" name="email" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Adminrechte:</td>
+        <td>
+            <input name="Admin" size="Adminrechte" type="checkbox"/>
+        </td>
+    </tr>
+    
+    <tr>
+        <td><input type="submit" name="Speichern" value="erfassen" /></td>
+        <td><input type="reset" value="Zurücksetzen" /></td>
+    </tr>
+    </tbody>
+    </table>
+</form>
+</body>
+</html>
---
+++ Simon_Forms/Liegenschaft_form.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <form action="Rechnung_form.php" method="POST">
+    <h2>Liegenschaft erfassen</h2>
+    <table border="0" cellspacing="0" cellpadding="2">
+    <tbody>
+    <tr>
+        <td>Rechnungstyp:</td>
+        <td>
+    <select name="top5">
+        <option>Reperatur Allgemein</option>
+        <option>Reperatur individuell</option>
+        <option>Hauswart</option>
+        <option>Heizkosten</option>
+        <option>Strom Allgemein</option>
+        <option>Strom Individuell</option>
+        <option>Wasser</option>
+        <option>Andere</option>
+    </select>
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Rechnungsdatum:*</td>
+        <td>
+            <input type="date" name="rechnungsdatum" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Rechnungsnummer:*</td>
+        <td>
+           <input type="number" name="rechnungsnummer" value="" /> 
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Betrag:*</td>
+        <td>
+           <input type="number" name="betrag" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Mieter:</td>
+        <td>
+           <select name="top5">
+            <option> </option>
+            </select>
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Zahlung Vermieter:</td>
+        <td>
+           <input type="number" name="zahlungVermieter" value="" />
+        </td>
+    </tr>
+    
+    <tr>
+        <td>Bemerkung:</td>
+        <td>
+           <textarea type="text" name="bemerkung" value=""> </textarea>
+        </td>
+    </tr>
+    
+    <tr>
+        <td><input type="submit" name="erfassen" value="erfassen" /></td>
+        <td><input type="reset" value="nochmals" /></td>
+    </tr>
+    </tbody>
+    </table>
+</form>
+</body>
+</html>
