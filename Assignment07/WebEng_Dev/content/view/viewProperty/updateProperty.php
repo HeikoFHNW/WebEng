343deff73cd2b16478756925f2f8909aa54b10e0
bobsburgers11 <simon.zahnd@students.fhnw.ch>
2016-12-06 08:00:09 +0100

    update property js works

--- Assignment07/WebEng_Dev/content/view/viewProperty/updateProperty.php
+++ Assignment07/WebEng_Dev/content/view/viewProperty/updateProperty.php
@@ -1,3 +1,4 @@
+<?php include ("../../content/includes/head.inc.php");?>
 <div class="container">
 
     <div class="span10 offset1">
@@ -5,7 +6,7 @@
             <h3>Update a Property</h3>
         </div>
 
-        <form class="form-horizontal" action="?controller=Property&action=update&id_property=<?php echo $property->getId_property() ?>" method="post">
+        <form class="form-horizontal" action="?controller=Property&action=update&id_property=<?php echo $property->getId_property() ?>" onkeyup="return validatePropertyForm()" method="post">
             <?php include_once("formProperty.php"); ?>
             <div class="form-actions">
                 <button type="submit" class="btn btn-success">Update</button>
@@ -15,3 +16,40 @@
     </div>
 
 </div>
+
+<script>
+        function validatePropertyForm(){
+  
+                
+                var a = (notEmpty(fP_strasse)&&isText(fP_strasse));
+                var b = (notEmpty(fP_ort)&&isText(fP_ort));
+                var c = (notEmpty(fP_plz)&&isNumber(fP_plz));
+                var d = notEmpty(fP_strassennummer);
+               
+            
+            
+           if (a){
+                isNotTextMessageStreet.innerHTML = "";
+            }else{
+                isNotTextMessageStreet.innerHTML = "Bitte keine Zahlen verwenden.";
+            }
+            
+            if (b){
+                isNotTextMessageOrt.innerHTML = "";
+            }else{
+                isNotTextMessageOrt.innerHTML = "Bitte keine Zahlen verwenden.";
+            }
+            
+            if (c){
+                plzMessage.innerHTML = "";
+            }else{
+                plzMessage.innerHTML = "Bitte keine Buchstaben.";
+            }
+           
+            if (a&&b&&c&&d){
+                return true;
+            }else{
+                return false;
+            }  
+        }
+        </script>
